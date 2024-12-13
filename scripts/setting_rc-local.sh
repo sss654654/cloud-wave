0a7357d0d784777e6deac37ac20fe20fc9e1d41e
Workshop Public Cloud <cj-cloud-wave@workshops.aws>
2024-12-13 16:00:07 +0000

    update rc-local file

--- scripts/setting_rc-local.sh
+++ scripts/setting_rc-local.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 # Configuration to restart streamlit application 
-touch /etc/rc.d/rc.local
-cat <<EOT >> /etc/rc.d/rc.local
-#!/bin/bash
-streamlit run /root/streamlit-project/main.py --server.port 80
-EOT
+sudo touch /etc/rc.d/rc.local
+sudo echo '#!/bin/bash
+streamlit run /home/ec2-user/streamlit-project/main.py --server.port 8080 --server.enableCORS false --server.enableXsrfProtection false --server.enableWebsocketCompression=false' | sudo tee /etc/rc.d/rc.local
 
-ln -s /etc/rc.d/rc.local /etc/rc.local
-chmod 755 /etc/rc.d/rc.local
+sudo ln -s /etc/rc.d/rc.local /etc/rc.local
+sudo chmod 755 /etc/rc.d/rc.local
 
-# Configuration System demon 
-echo " " >> /lib/systemd/system/rc-local.service 
-echo "[Install]" >> /lib/systemd/system/rc-local.service 
-echo "WantedBy=multi-user.target" >> /lib/systemd/system/rc-local.service 
+# Configuration Systemd Service
+sudo bash -c 'echo "Restart=always" >> /lib/systemd/system/rc-local.service'
+sudo bash -c 'echo "User=ec2-user" >> /lib/systemd/system/rc-local.service'
+sudo bash -c 'echo " " >> /lib/systemd/system/rc-local.service'
+sudo bash -c 'echo "[Install]" >> /lib/systemd/system/rc-local.service '
+sudo bash -c 'echo "WantedBy=multi-user.target" >> /lib/systemd/system/rc-local.service '
 
-# Install Code-Deploy Agent
-systemctl daemon-reload
-systemctl enable rc-local
-
-yum install ruby -y
-cd /home/ec2-user
-wget https://aws-codedeploy-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/latest/install
-chmod +x install
-./install auto
-service codedeploy-agent status
+# Service enable
+sudo systemctl daemon-reload
+sudo systemctl enable rc-local
