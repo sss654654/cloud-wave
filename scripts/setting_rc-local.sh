748a77ea0a19bfdd0a8ef247ec689b1c40814504
Workshop Public Cloud <cj-cloud-wave@workshops.aws>
2024-12-14 13:46:41 +0000

    update rc-local

--- scripts/setting_rc-local.sh
+++ scripts/setting_rc-local.sh
@@ -2,7 +2,7 @@
 # Configuration to restart streamlit application 
 sudo touch /etc/rc.d/rc.local
 sudo echo '#!/bin/bash
-streamlit run /root/streamlit-project/main.py --server.port 80
+streamlit run /root/streamlit-project/main.py --server.port 80'
 
 sudo ln -s /etc/rc.d/rc.local /etc/rc.local
 sudo chmod 755 /etc/rc.d/rc.local
