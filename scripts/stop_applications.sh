4ea18dae386233ed5ad97674985c3b79ca0fb7de
sh1517.you <sh1517.you@gmail.com>
2025-01-09 14:31:53 +0000

    update

--- scripts/stop_applications.sh
+++ scripts/stop_applications.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+
 terminate_process() {
   local PIDS=$1
   local NAME=$2
+  local NOW=$(date "+%Y %b %d %a %H:%M:%S")
+  local STOP_LOG="/var/log/codedeploy_$NAME.log"
 
   if [ -z "$PIDS" ]; then
-    echo "$NAME is not running"
+    echo "[$NOW] $NAME is not running" >> $STOP_LOG
   else
+    echo "[$NOW] 기존 프로세스 종료 시도: PID $PIDS" >> $STOP_LOG
     for PID in $PIDS; do
       echo "Kill -9 $PID ($NAME)"
       kill -9 $PID
@@ -20,8 +24,8 @@ APP_PID=$(pgrep streamlit)
 FLASK_PIDS=$(pgrep -f "python app.py")
 
 # Terminate processes
-terminate_process "$APP_PID" "Streamlit"
-terminate_process "$FLASK_PIDS" "Flask"
+terminate_process "$APP_PID" "streamlit"
+terminate_process "$FLASK_PIDS" "flask"
 
 sleep 5
 exit 0
