e060307e4407b2e6a950695f8ab866519480f05d
Workshop Public Cloud <subinhong0109@dankook.ac.kr>
2025-07-24 08:42:24 +0000

    update main.py, appspec.yml, db_connection.py

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ def side_bar():
         selected_page = option_menu(
             menu_title="Main Menu", 
             # options=["Main_Home"],
-            options=["Main_Home", "Photo_Gallery"],
-            # options=["Main_Home", "Photo_Gallery", "Travel_Attracstions"],
+            # options=["Main_Home", "Photo_Gallery"],
+            options=["Main_Home", "Photo_Gallery", "Travel_Attracstions"],
             icons=['bi bi-house', 'bi bi-images', 'bi bi-airplane-engines'],
             menu_icon="cast", 
             default_index=0,
